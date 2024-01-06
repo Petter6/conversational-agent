040a1017c6aaea4d03273a22bb24934e2bd26ba6
NathanDWasTaken <derna541@hotmail.com>
2024-01-06 12:02:08 +0100

    start AEM

---
+++ AEM/AEM.py
@@ -0,0 +1,69 @@
+"""
+Terms:
+
+(Current) Situation:
+    - state + activity
+
+
+
+TODO:
+    - Think of representation for Episode
+
+Problems:
+    - everything is very abstract
+"""
+
+
+
+class Episode:
+    """
+    Experiences/Situations encoded in a way that allows distances to other Episodes to be calculated
+    """
+
+
+class EmotionalQuality:
+    """
+    Emotional quality of an episode
+    """
+
+
+
+class EpisodicMemory:
+    """
+    Stores a user's personal events from their past, encoded as Episodes
+    """
+
+
+
+
+class GoalRepository:
+    """
+    Stores a user's existing goals/motivations and concerns
+    """
+
+
+em = EpisodicMemory()
+gr = GoalRepository()
+
+
+
+
+def ecphoricProcessing(state, activity):
+    """
+    Convert current situation into one or more Episodes
+    Determines associative strength (distance) between that Episode and all other Episodes stored in Memory
+
+    """
+    em
+
+
+
+
+
+def emotionalAppraisal(episode) -> EmotionalQuality:
+    """
+    Determine emotional quality of experiencing an Episode based on user's Goal Repository
+    """
+    gr
+
+
