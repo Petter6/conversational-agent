9271d9d7a1043ddac845e6ae998dbaecb2fde5c1
NathanDWasTaken <derna541@hotmail.com>
2024-01-19 16:32:23 +0100

    add Cost Of Living matrices

---
+++ AEM/CoL.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# Desired CoL at destination: -> Low, Medium, High
+# Current CoL
+# |
+# v
+# Low
+# Medium
+# High
+
+col_matrix = np.array([
+    [1,     1.2,    1.4],
+    [1.2,   1.4,    1.6],
+    [1.4,   1.75,   2]
+])
+# -> means of the person
+
+
+# Distance -> <1 week, <2 weeks, <4 weeks, >4 weeks
+# Duration:
+# |
+# v
+# <1000
+# <6000
+# >6000
+
+dist_matrix = np.array([
+    [20,    10,     0,      0],
+    [50,    30,     10,     5],
+    [100,   70,     50,     20]
+])
+# -> Limit CoL of travel city (any cities over this CoL are too expensive)
