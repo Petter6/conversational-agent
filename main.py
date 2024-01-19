6a94bb523afc419b33541970cbd213b9004dafad
Petter <petter.reijalt@gmail.com>
2024-01-19 10:02:58 +0100

    Logic

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from furhat_remote_api import FurhatRemoteAPI
 from transformers import pipeline
 import cv2
 from fer import FER
-import time
+import spacy
+import numerizer
 
 
 def analyze_emotion(text):
@@ -18,45 +19,22 @@ def analyze_emotion(text):
 
 def listen():
     text = furhat.listen()
-    return text
+    return text.message
 
 
 def video():
-    recording_time = 1
     # Initialize the camera
     cap = cv2.VideoCapture(0)  # 0 is usually the default value for the primary camera
 
     # Initialize the FER detector
     detector = FER()
 
-    start_time = time.time()
-    last_detection_time = start_time
+    ret, frame = cap.read()
 
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
+    if not ret:
+        return
 
-        if not ret:
-            break
-
-        # Display the resulting frame
-        # cv2.imshow('Webcam', frame)
-
-        current_time = time.time()
-        elapsed_time = current_time - start_time
-
-        # Perform emotion detection every second
-        if current_time - last_detection_time >= 1:
-            last_detection_time = current_time
-            emotions = detector.detect_emotions(frame)
-
-        # Break the loop after the specified recording time
-        if elapsed_time > recording_time:
-            break
-
-        # Press Q to exit before the specified recording time
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    emotions = detector.detect_emotions(frame)
 
     # When everything is done, release the capture
     cap.release()
@@ -92,27 +70,7 @@ def parse_video_emotions(data):
     return data[0]['emotions']
 
 
-if __name__ == '__main__':
-    # Create an instance of the FurhatRemoteAPI class
-    furhat = FurhatRemoteAPI("localhost")
-
-    # Say "Hi there, how are you?"
-    furhat.say(text="Hi there, how are you?", blocking=True)
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future1 = executor.submit(listen)
-        future2 = executor.submit(video)
-
-        result1 = future1.result()
-        result2 = future2.result()
-
-    text_emotion = analyze_emotion(result1.message)
-    video_emotion = result2
-
-    # Parsing
-    text_emotions_parsed = parse_text_emotions(text_emotion)
-    video_emotions_parsed = parse_video_emotions(video_emotion)
-
+def get_combined_score(text_emotions, video_emotions):
     emotion_mapping = {
         'joy': 'happy',
         'sadness': 'sad',
@@ -122,18 +80,102 @@ if __name__ == '__main__':
     }
 
     # Calculating the combined emotional scores
-    combined_emotions = {}
+    emotions_array = {}
     text_weight = 0.8  # Weight for text analysis results
     video_weight = 0.2  # Weight for video analysis results
 
-    for text_emotion, text_score in text_emotions_parsed.items():
-        if text_emotion in emotion_mapping:
-            video_emotion = emotion_mapping[text_emotion]
-            combined_score = text_score * text_weight + video_emotions_parsed.get(video_emotion, 0) * video_weight
+    for emotion, text_score in text_emotions.items():
+        if emotion in emotion_mapping:
+            video_score = emotion_mapping[emotion]
+            combined_score = text_score * text_weight + video_emotions.get(video_score, 0) * video_weight
         else:
             # If the emotion is not present in the video analysis, use only the text score
             combined_score = text_score * text_weight
 
-        combined_emotions[text_emotion] = combined_score
+        emotions_array[emotion] = combined_score
 
-    print(combined_emotions)
+    return emotions_array
+
+
+def get_answer():
+    text = furhat.listen()
+
+    while not text.message:
+        furhat.say(
+            text="Sorry I couldn't understand, could you repeat?", blocking=True)
+        text = furhat.listen()
+
+    return text.message
+
+
+def get_answer_and_emotion():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future1 = executor.submit(listen)
+        future2 = executor.submit(video)
+
+        result1 = future1.result()
+        result2 = future2.result()
+
+    text_emotion = analyze_emotion(result1)
+    video_emotion = result2
+
+    # Parsing
+    text_emotions_parsed = parse_text_emotions(text_emotion)
+    video_emotions_parsed = parse_video_emotions(video_emotion)
+
+    combined_emotions = get_combined_score(text_emotions_parsed, video_emotions_parsed)
+
+    return result1, combined_emotions
+
+
+def get_budget():
+    doc = nlp(get_answer())
+    positive_patterns = ['yes', 'sure', 'absolutely', 'agree', 'like', 'correct', 'yeah']
+
+    budget_items = list(doc._.numerize().items())
+
+    while not budget_items:
+        furhat.say(text=f"Can you repeat your budget", blocking=True)
+        doc = nlp(get_answer())
+        budget_items = list(doc._.numerize().items())
+
+    budget = budget_items[0][1]
+    furhat.say(text=f"So your budget is {budget}?", blocking=True)
+
+    while True:
+        doc = nlp(get_answer())
+        for token in doc:
+            if token.text.lower() in positive_patterns:
+                return budget
+
+        while not doc.has_extension("numerize"):
+            furhat.say(text=f"Can you repeat your budget", blocking=True)
+            doc = nlp(get_answer())
+
+        budget_items = list(doc._.numerize().items())
+
+        while not budget_items:
+            furhat.say(text=f"Can you repeat your budget", blocking=True)
+            doc = nlp(get_answer())
+            budget_items = list(doc._.numerize().items())
+
+        budget = budget_items[0][1]
+        furhat.say(text=f"So your budget is {budget}?", blocking=True)
+
+
+if __name__ == '__main__':
+    nlp = spacy.load("en_core_web_md")
+
+    # Create an instance of the FurhatRemoteAPI class
+    furhat = FurhatRemoteAPI("localhost")
+
+    furhat.say(text="Hi there, my name is Matthew, I'm going to help you find your next vacation destination. What is "
+                    "your name?", blocking=True)
+    name = get_answer()
+
+    furhat.say(text=f"Hi {name}, what is your budget?", blocking=True)
+    total_budget = get_budget()
+
+    furhat.say(text=f"have you been to any beach towns when did you go and how did you find it", blocking=True)
+    response = get_answer_and_emotion()
+    print(response)
